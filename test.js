08b3c88ff35ba870b362597cbd15b61cba91cf53
POKRYWA Lucas <lucas.pokrywa@gmail.com>
2023-10-12 11:50:28 +0200

    ajout du js en force

--- test.js
+++ test.js
@@ -9,6 +9,67 @@ console.log("La somme fait : " + resultat);
 console.log("La somme fait : " + val1 + val2);
 
 
-function Mon_popup(){
+/*function Mon_popup(){
     alert("Gestion de l'evenement 'mouse over' sur mon bouton");
 }
+*/
+function TrameValide(){
+    var trameHexa = document.getElementById("edit");
+    var trame = trameHexa.value;
+    var button = document.getElementById("boutton");
+    button.disable = false;
+    var queDeLHexa = true;
+    var respecttaille = true
+    console.log("C" > "B")
+    
+        if (trame.length%2==0){
+            document.getElementById("longueur").className = "vert" ;
+            console.log(document.getElementById("longueur").className + "lenght");
+            respecttaille = true;
+        }
+        else {
+            document.getElementById("longueur").className = "rouge" ;
+            console.log(document.getElementById("longueur").className  +  "lenght");
+            respecttaille = false;
+        }
+
+   
+        for (let i = 0; i < trame.length; i++) {
+            if (trame[i] >= 0 && trame[i] <= 9 || trame.charAt(i) >= "A" && trame.charAt(i) <= "F")
+            {
+                document.getElementById("hexa").className = "vert" ;
+                console.log("bon");
+                queDeLHexa = true;
+            }
+            else
+            {
+                console.log("pas bon " + trame[i]);
+                document.getElementById("hexa").className = "rouge" ;
+                queDeLHexa = false;
+                break;
+            }
+        }
+
+     
+        if (queDeLHexa == false || respecttaille == false ) {
+            console.log("couleur en" + document.getElementById("longueur").className);
+            button.disabled = true
+        }
+        else {
+            console.log("couleur en" + document.getElementById("longueur").className);
+            button.disabled = false;
+        }
+    
+}
+
+
+
+var s1 = document.getElementById("scrollbar1");
+
+s1.addEventListener('change', function () 
+{ 
+    document.getElementById("edit").value= s1.value.toString(16)
+}, false);
+
+
+
